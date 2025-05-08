7fd193ac8184b098e0c8369014324d83e61b39af
Henri <henrilorilleux@gmail.com>
2025-05-08 18:08:07 +0200

    modification du schéma persons

--- models/persons.js
+++ models/persons.js
@@ -6,10 +6,10 @@ const personsSchema = mongoose.Schema({
   birthday: Date,
   dead: Boolean,
   deathdate: Date,
-  isMarried: Boolean,
-  conjointId: { type: mongoose.Schema.Types.ObjectId, ref: 'persons' },
+  estMarie: Boolean,
+  idConjoint: { type: mongoose.Schema.Types.ObjectId, ref: 'persons' },
   isBornFamily: Boolean,
-  familyId: { type: mongoose.Schema.Types.ObjectId, ref: 'mariages' },
+  idFamille: { type: mongoose.Schema.Types.ObjectId, ref: 'mariages' },
 }, { timestamps: true });
 
 const Person = mongoose.model('person', personsSchema);
